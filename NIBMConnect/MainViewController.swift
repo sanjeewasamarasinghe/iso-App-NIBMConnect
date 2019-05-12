c29308262a95491fa5ae13f208847f9e10d83034
sanjeewa samarasinghe <sanjeewasonline@gmail.com>
2019-05-12 10:04:55 +0530

    add storyboard login & singout

--- NIBMConnect/MainViewController.swift
+++ NIBMConnect/MainViewController.swift
@@ -11,13 +11,25 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var MainNext: UIButton!
+    @IBOutlet weak var MainSingIn: UIButton!
+    @IBOutlet weak var singIn: UIButton!
+    @IBOutlet weak var SingInOut: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func singInOutBtn(_ sender: Any) {
+        self.performSegue(withIdentifier: "singInOut", sender: nil)
+    }
     
-
+    @IBAction func singInBtn(_ sender: Any) {
+        self.performSegue(withIdentifier: "singInOut", sender: nil)
+    }
+    @IBAction func showSingInPage(_ sender: Any) {
+        self.performSegue(withIdentifier: "singInOut", sender: nil)
+    }
+    
     @IBAction func showNext(_ sender: Any) {
         self.performSegue(withIdentifier: "LogIn", sender: nil)
     }
