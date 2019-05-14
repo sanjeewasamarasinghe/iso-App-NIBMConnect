23649a0344d3dd22ea4d613284685c03f650e915
sanjeewa samarasinghe <sanjeewasonline@gmail.com>
2019-05-14 23:08:11 +0530

    Add my profile page

--- NIBMConnect/MyProfile.swift
+++ NIBMConnect/MyProfile.swift
@@ -7,16 +7,99 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class MyProfile: UIViewController {
 
+    @IBOutlet weak var profilrPic: UIImageView!
+    @IBOutlet weak var lastName: UILabel!
+    @IBOutlet weak var fristName: UILabel!
+    
+    
+    
+    var connectList = [ConnectModel]()
+    var ref:DatabaseReference!
+    
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        
+        self.ref.child("NIBM").observeSingleEvent(of: .value, with: {(snapshot)in
+            if snapshot.childrenCount>0{
+                //self.connectList.removeAll();
+                for Data in snapshot.children.allObjects as! [DataSnapshot]{
+                    let dataObject = Data.value as? [String:AnyObject  ]
+                    let FristName = dataObject? ["FristName"]
+                    let LastName = dataObject? ["LastName"]
+                    let NIC = dataObject? ["NIC"]
+                    let AddressLineOne = dataObject? ["AddressLineOne"]
+                    let AddressLineTwo = dataObject? ["AddressLineTwo"]
+                    let City = dataObject? ["City"]
+                    let PhoneNumber = dataObject? ["PhoneNumber"]
+                    let Email = dataObject? ["Email"]
+                    let ProfilePicture = dataObject? ["ProfilePicture"]
+                    let FBProfileUrl = dataObject? ["FBProfileUrl"]
+                    
+                    let DataOject = ConnectModel (FristName:FristName as! String?,
+                                                  LastName:LastName as! String?,
+                                                  NIC:NIC as! String?,
+                                                  AddressLineOne:AddressLineOne as! String?,
+                                                  AddressLineTwo:AddressLineTwo as! String?,
+                                                  City:City as! String?,
+                                                  PhoneNumber:PhoneNumber as! String?,
+                                                  Email:Email as! String?,
+                                                  ProfilePicture:ProfilePicture as! String?,
+                                                  FBProfileUrl:FBProfileUrl as! String?)
+                    self.connectList.append(DataOject)
+                }
+                let  user = Auth.auth().currentUser
+                self.fristName.text=user?.email
+                var count = String(self.connectList.count)
+                var Object :ConnectModel!
+               
+                //for obj in self.connectList{
+                    //if (obj.Email == user?.email){
+                        //Object=obj
+                         //self.lastName.text="f"
+                        
+                    //}else{
+                        //self.lastName.text="n"
+                    //}
+                //}
+                
+                let ob = self.connectList.first{$0.Email==user?.email}
+                if ((ob) != nil){
+                Object=ob
+                    
+                self.lastName.text=Object.Email
+                
+                }else{
+                self.lastName.text="n"
+                }
+            }
+        })
+        
+        ref.observe(DataEventType.value, with: {(snapshot)in
+            
+        })
+        let  user = Auth.auth().currentUser
+        self.fristName.text=user?.email
+        
+      
+        
+       
     }
+   
     
-
+    
+    
+  
     /*
     // MARK: - Navigation
 
